6473d63d2b1b90002c6d241960f437f964c4ea09
Piolo Torrecampo <94138318+piolo-torrecampo@users.noreply.github.com>
2025-02-21 07:59:49 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,8 @@
 import { updateSession } from '@/utils/supabase/middleware';
-import { NextResponse, type NextRequest } from 'next/server';
-import { createClientForServer } from '@/utils/supabase/server';
+import { type NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const response = await updateSession(request);
-
-  const supabase = await createClientForServer();
-  const { data: { session } } = await supabase.auth.getSession();
-
-  const protectedRoutes = ['/dashboard'];
-  const authRoutes = ['/login']; 
-
-  const pathname = request.nextUrl.pathname;
-
-  if (protectedRoutes.includes(pathname) && !session) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  if (authRoutes.includes(pathname) && session) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
-  }
+  const response = await updateSession(request); 
 
   return response;
 }
