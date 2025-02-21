46e8dd87f52879da5856b675ed912b7776b97cf4
Piolo Torrecampo <94138318+piolo-torrecampo@users.noreply.github.com>
2025-02-21 10:28:26 +0800

    updated

--- src/app/dashboard/edit/page.tsx
+++ src/app/dashboard/edit/page.tsx
@@ -1,14 +1,14 @@
-'use client'
- 
-import { Suspense } from 'react'
- 
+'use client';
+
+import { Suspense } from 'react';
+import EditBlogForm from '@/components/edit/edit-form';
+
 const EditBlogPage = () => {
   return (
-    <Suspense>
-      <EditBlogPage />
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditBlogForm />
     </Suspense>
-  )
-}
+  );
+};
 
-export default EditBlogPage
-
+export default EditBlogPage;
